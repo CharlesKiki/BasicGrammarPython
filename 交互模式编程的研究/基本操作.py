df6d27e53545c939f1e47ed62a6295c09ee465f5
Charles <HUCharles@foxmail.com>
2019-02-24 14:31:36 +0800

    <交互式编程的例子> 1.从命令行复制的全过程 2.交互式编程的引用场景，README

---
+++ 交互模式编程的研究/基本操作.py
@@ -0,0 +1,13 @@
+# 该代码文件还原了在命令行界面的编程过程，但是这不意味着可以直接运行
+# 该文件仅仅用来描述如何交互式编程，以及，需要注意的事项
+# 注释方式依旧采用文件编程的方式进行
+
+>>> flag = True
+#注意True是Python的一个内置关键字
+>>> if flag :
+...     print("Now print something")
+...
+Now print something
+#当输入if之后没有更多语句之后可以再按一次回车继续执行程序
+>>> from tkinter import *
+#引入外部模块的方式和文件编程相同
